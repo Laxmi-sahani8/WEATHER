04e608654835ea763faf72655bf5e29b72c46bad
Laxmi-sahani8 <165988964+Laxmi-sahani8@users.noreply.github.com>
2024-10-23 11:14:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherGet from './weather/WeatherGet'
+
+const App = () => {
+  return (
+    <div>
+      <WeatherGet/>
+    </div>
+  )
+}
+
+export default App
